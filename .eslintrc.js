6efcdd3898ae4e7a71fbee860f23a4f693a38386
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-06-09 17:02:11 -0600

    Add async to axios (2Table)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    'eslint:recommended',
+    "extends": "plugin:vue/base",
+    "extends": "plugin:vue/essential",
+    'plugin:vue/recommended',
+      "extends": "plugin:vue/strongly-recommended"
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
